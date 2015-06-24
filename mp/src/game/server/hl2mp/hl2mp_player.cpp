3e0a12d3ec5f1b894d73a48eba4a745c03484838
soft as HELL <softashell@cock.li>
2015-06-24 18:19:39 +0300

    Give default weapons

--- mp/src/game/server/hl2mp/hl2mp_player.cpp
+++ mp/src/game/server/hl2mp/hl2mp_player.cpp
@@ -198,37 +198,24 @@ void CHL2MP_Player::GiveDefaultItems( void )
 {
 	EquipSuit();
 
+	// Give ammo
 	CBasePlayer::GiveAmmo( 255,	"Pistol");
-	CBasePlayer::GiveAmmo( 45,	"SMG1");
-	CBasePlayer::GiveAmmo( 1,	"grenade" );
-	CBasePlayer::GiveAmmo( 6,	"Buckshot");
-	CBasePlayer::GiveAmmo( 6,	"357" );
 
-	if ( GetPlayerModelType() == PLAYER_SOUNDS_METROPOLICE || GetPlayerModelType() == PLAYER_SOUNDS_COMBINESOLDIER )
-	{
-		GiveNamedItem( "weapon_stunstick" );
-	}
-	else if ( GetPlayerModelType() == PLAYER_SOUNDS_CITIZEN )
-	{
-		GiveNamedItem( "weapon_crowbar" );
-	}
-	
-	GiveNamedItem( "weapon_pistol" );
-	GiveNamedItem( "weapon_smg1" );
-	GiveNamedItem( "weapon_frag" );
-	GiveNamedItem( "weapon_physcannon" );
+	// Give default weapons
+	GiveNamedItem("weapon_knife");
 
 	const char *szDefaultWeaponName = engine->GetClientConVarValue( engine->IndexOfEdict( edict() ), "cl_defaultweapon" );
 
 	CBaseCombatWeapon *pDefaultWeapon = Weapon_OwnsThisType( szDefaultWeaponName );
 
+	// Switch to default weapon
 	if ( pDefaultWeapon )
 	{
 		Weapon_Switch( pDefaultWeapon );
 	}
 	else
 	{
-		Weapon_Switch( Weapon_OwnsThisType( "weapon_physcannon" ) );
+		Weapon_Switch( Weapon_OwnsThisType( "weapon_knife" ) );
 	}
 }
 
