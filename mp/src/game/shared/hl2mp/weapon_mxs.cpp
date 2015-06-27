4da78b0afe64f53e8f3ac9a8649ae734f76e6962
Rainyan <cyberiatexture@gmail.com>
2015-06-27 22:26:35 +0300

    Add MX and MXs

---
+++ mp/src/game/shared/hl2mp/weapon_mxs.cpp
@@ -0,0 +1,124 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: MXs assault rifle
+//
+//=============================================================================//
+
+#include "cbase.h"
+
+#ifdef CLIENT_DLL
+	#include "c_hl2mp_player.h"
+#else
+	#include "hl2mp_player.h"
+#endif
+
+#include "weapon_hl2mpbase.h"
+#include "weapon_hl2mpbase_machinegun.h"
+
+#ifdef CLIENT_DLL
+#define CWeaponMXs C_WeaponMXs
+#endif
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+class CWeaponMXs : public CHL2MPMachineGun
+{
+public:
+	DECLARE_CLASS( CWeaponMXs, CHL2MPMachineGun );
+
+	CWeaponMXs();
+
+	DECLARE_NETWORKCLASS(); 
+	DECLARE_PREDICTABLE();
+
+	void	AddViewKick( void );
+
+	int		GetMinBurst() { return 2; }
+	int		GetMaxBurst() { return 5; }
+
+	float	GetFireRate( void ) { return 0.075f; }	// 13.3hz
+	Activity	GetPrimaryAttackActivity( void );
+
+	virtual const Vector& GetBulletSpread( void )
+	{
+		static const Vector cone = VECTOR_CONE_5DEGREES;
+		return cone;
+	}
+
+#ifndef CLIENT_DLL
+	DECLARE_ACTTABLE();
+#endif
+	
+private:
+	CWeaponMXs( const CWeaponMXs & );
+};
+
+IMPLEMENT_NETWORKCLASS_ALIASED( WeaponMXs, DT_WeaponMXs )
+
+BEGIN_NETWORK_TABLE( CWeaponMXs, DT_WeaponMXs )
+END_NETWORK_TABLE()
+
+BEGIN_PREDICTION_DATA( CWeaponMXs )
+END_PREDICTION_DATA()
+
+LINK_ENTITY_TO_CLASS( weapon_mxs, CWeaponMXs );
+PRECACHE_WEAPON_REGISTER(weapon_mxs);
+
+#ifndef CLIENT_DLL
+acttable_t	CWeaponMXs::m_acttable[] = 
+{
+	{ ACT_HL2MP_IDLE,					ACT_HL2MP_IDLE_SMG1,					false },
+	{ ACT_HL2MP_RUN,					ACT_HL2MP_RUN_SMG1,						false },
+	{ ACT_HL2MP_IDLE_CROUCH,			ACT_HL2MP_IDLE_CROUCH_SMG1,				false },
+	{ ACT_HL2MP_WALK_CROUCH,			ACT_HL2MP_WALK_CROUCH_SMG1,				false },
+	{ ACT_HL2MP_GESTURE_RANGE_ATTACK,	ACT_HL2MP_GESTURE_RANGE_ATTACK_SMG1,	false },
+	{ ACT_HL2MP_GESTURE_RELOAD,			ACT_HL2MP_GESTURE_RELOAD_SMG1,			false },
+	{ ACT_HL2MP_JUMP,					ACT_HL2MP_JUMP_SMG1,					false },
+	{ ACT_RANGE_ATTACK1,				ACT_RANGE_ATTACK_SMG1,					false },
+};
+
+IMPLEMENT_ACTTABLE(CWeaponMXs);
+#endif
+
+//=========================================================
+CWeaponMXs::CWeaponMXs( )
+{
+
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output : Activity
+//-----------------------------------------------------------------------------
+Activity CWeaponMXs::GetPrimaryAttackActivity( void )
+{
+	if ( m_nShotsFired < 2 )
+		return ACT_VM_PRIMARYATTACK;
+
+	if ( m_nShotsFired < 3 )
+		return ACT_VM_RECOIL1;
+	
+	if ( m_nShotsFired < 4 )
+		return ACT_VM_RECOIL2;
+
+	return ACT_VM_RECOIL3;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponMXs::AddViewKick( void )
+{
+	#define	EASY_DAMPEN			0.5f
+	#define	MAX_VERTICAL_KICK	1.0f	//Degrees
+	#define	SLIDE_LIMIT			2.0f	//Seconds
+	
+	//Get the view kick
+	CBasePlayer *pPlayer = ToBasePlayer( GetOwner() );
+
+	if ( pPlayer == NULL )
+		return;
+
+	DoMachineGunKick( pPlayer, EASY_DAMPEN, MAX_VERTICAL_KICK, m_fFireDuration, SLIDE_LIMIT );
+}
