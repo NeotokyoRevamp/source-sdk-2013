090f2971e5c052f236eb5fbe7bce89b387e5b570
soft as HELL <softashell@cock.li>
2015-07-03 14:42:37 +0300

    Parse viewmodel offsets from weapons
    Could probably do it in better way but for now I just used same values
    in script as neotokyo originally did

--- mp/src/game/shared/hl2mp/hl2mp_weapon_parse.cpp
+++ mp/src/game/shared/hl2mp/hl2mp_weapon_parse.cpp
@@ -14,19 +14,40 @@ FileWeaponInfo_t* CreateWeaponInfo()
 	return new CHL2MPSWeaponInfo;
 }
 
-
-
 CHL2MPSWeaponInfo::CHL2MPSWeaponInfo()
 {
 	m_iPlayerDamage = 0;
-}
 
+	// Viewmodel offset
+	m_flVMFov = 54.0;
+	m_flVMForward = 0.0;
+	m_flVMRight = 0.0;
+	m_flVMUp = 0.0;
+	m_flVMYaw = 0.0;
+
+	// Viewmodel aiming offset
+	m_flVMAimFov = 54.0;
+	m_flVMAimForward = 0.0;
+	m_flVMAimRight = 0.0;
+	m_flVMAimUp = 0.0;
+}
 
 void CHL2MPSWeaponInfo::Parse( KeyValues *pKeyValuesData, const char *szWeaponName )
 {
 	BaseClass::Parse( pKeyValuesData, szWeaponName );
 
-	m_iPlayerDamage = pKeyValuesData->GetInt( "damage", 0 );
+	m_iPlayerDamage = pKeyValuesData->GetInt("damage", 0);
+
+	m_flVMFov = pKeyValuesData->GetFloat("VMFov");
+	m_flVMForward = pKeyValuesData->GetFloat("VMOffsetForward");
+	m_flVMRight = pKeyValuesData->GetFloat("VMOffsetRight");
+	m_flVMUp = pKeyValuesData->GetFloat("VMOffsetUp");
+	m_flVMYaw = pKeyValuesData->GetFloat("VMAngleYaw");
+
+	m_flVMAimFov = pKeyValuesData->GetFloat("VMAimFov");
+	m_flVMAimForward = pKeyValuesData->GetFloat("VMAimOffsetForward");
+	m_flVMAimRight = pKeyValuesData->GetFloat("VMAimOffsetRight");
+	m_flVMAimUp = pKeyValuesData->GetFloat("VMAimOffsetUp");
 }
 
 
--- mp/src/game/shared/hl2mp/hl2mp_weapon_parse.h
+++ mp/src/game/shared/hl2mp/hl2mp_weapon_parse.h
@@ -29,6 +29,17 @@ public:
 public:
 
 	int m_iPlayerDamage;
+
+	float m_flVMFov;
+	float m_flVMForward;
+	float m_flVMRight;
+	float m_flVMUp;
+	float m_flVMYaw;
+
+	float m_flVMAimFov;
+	float m_flVMAimForward;
+	float m_flVMAimRight;
+	float m_flVMAimUp;
 };
 
 
