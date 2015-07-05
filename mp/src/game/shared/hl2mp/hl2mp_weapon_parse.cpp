d61a01f37f936273698f59210829f94115ab0c9e
soft as HELL <softashell@cock.li>
2015-07-05 21:24:13 +0300

    Initialize default values for classic sights

--- mp/src/game/shared/hl2mp/hl2mp_weapon_parse.cpp
+++ mp/src/game/shared/hl2mp/hl2mp_weapon_parse.cpp
@@ -18,17 +18,24 @@ CHL2MPSWeaponInfo::CHL2MPSWeaponInfo()
 {
 	m_iPlayerDamage = 0;
 
-	m_flVMFov = 54.0f; // Viewmodel fov
+	m_flVMFov = 54.0f; // Viewmodel fov without sights
+	m_flAimFov = 54.0f; //  Aimed viewmodel FOV with ironsigths
+	m_flZoomFov = 54.0f; //	Aimed viewmodel FOV with classic zoom
+
+	m_iAimType = 0; // 0 - no aiming, 1 - ironsights, 2 - scope
+	m_flAimZoom = 75.0f; // Game fov for zoom effect
+
+	// Default viewmodel offset
 	m_vecVMPosOffset = vec3_origin;
 	m_angVMAngOffset.Init();
 
-	m_iAimType = 0; // 0 - no aiming, 1 - ironsights, 2 - scope
-
-	m_flAimFov = 54.0f; // Aimed model FOV
-	m_flAimZoom = 75.0f; // Game fov for zoom effect
-
+	// Iron sight offset
 	m_vecAimPosOffset = vec3_origin;
 	m_angAimAngOffset.Init();
+
+	// Classic sight offset
+	m_vecZoomPosOffset = vec3_origin;
+	m_angZoomAngOffset.Init();
 }
 
 void CHL2MPSWeaponInfo::Parse( KeyValues *pKeyValuesData, const char *szWeaponName )
@@ -40,7 +47,7 @@ void CHL2MPSWeaponInfo::Parse( KeyValues *pKeyValuesData, const char *szWeaponNa
 	KeyValues *pViewModel = pKeyValuesData->FindKey("ViewModelData");
 	if (pViewModel)
 	{
-		m_flVMFov = pViewModel->GetFloat("fov", 0.0f);
+		m_flVMFov = pViewModel->GetFloat("fov", 54.0f);
 
 		m_vecVMPosOffset.x = pViewModel->GetFloat("forward", 0.0f);
 		m_vecVMPosOffset.y = pViewModel->GetFloat("right", 0.0f);
@@ -55,9 +62,9 @@ void CHL2MPSWeaponInfo::Parse( KeyValues *pKeyValuesData, const char *szWeaponNa
 	if (pIronSight)
 	{
 		m_iAimType = pIronSight->GetInt("type", 0);
-		m_flAimZoom = pIronSight->GetFloat("aimfov", 0.0f);
+		m_flAimZoom = pIronSight->GetFloat("aimfov", 75.0f);
 
-		m_flAimFov = pIronSight->GetFloat("fov", 0.0f);
+		m_flAimFov = pIronSight->GetFloat("fov", 54.0f);
 
 		m_vecAimPosOffset.x = pIronSight->GetFloat("forward", 0.0f);
 		m_vecAimPosOffset.y = pIronSight->GetFloat("right", 0.0f);
@@ -71,7 +78,7 @@ void CHL2MPSWeaponInfo::Parse( KeyValues *pKeyValuesData, const char *szWeaponNa
 	KeyValues *pZoom = pKeyValuesData->FindKey("ZoomData");
 	if (pZoom)
 	{
-		m_flZoomFov = pZoom->GetFloat("fov", 0.0f);
+		m_flZoomFov = pZoom->GetFloat("fov", 54.0f);
 
 		m_vecZoomPosOffset.x = pZoom->GetFloat("forward", 0.0f);
 		m_vecZoomPosOffset.y = pZoom->GetFloat("right", 0.0f);
