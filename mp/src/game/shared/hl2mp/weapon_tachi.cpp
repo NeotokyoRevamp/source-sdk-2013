bf92417e3c525bb8bc465daa8db5b8d24a52c1d3
soft as HELL <softashell@cock.li>
2015-07-07 22:05:47 +0300

    Remove most tachi specific code

--- mp/src/game/shared/hl2mp/weapon_tachi.cpp
+++ mp/src/game/shared/hl2mp/weapon_tachi.cpp
@@ -5,7 +5,6 @@
 //=============================================================================//
 
 #include "cbase.h"
-#include "in_buttons.h"
 
 #ifdef CLIENT_DLL
 	#include "c_hl2mp_player.h"
@@ -15,19 +14,9 @@
 
 #include "weapon_hl2mpbase.h"
 
-#define	TACHI_FASTEST_REFIRE_TIME		0.1f
-#define	TACHI_FASTEST_DRY_REFIRE_TIME	0.2f
-
-#define	TACHI_ACCURACY_SHOT_PENALTY_TIME		0.2f	// Applied amount of time each shot adds to the time we must recover from
-#define	TACHI_ACCURACY_MAXIMUM_PENALTY_TIME		2.0f	// Maximum penalty to deal out
-
 #ifdef CLIENT_DLL
 #define CWeaponTachi C_WeaponTachi
 #endif
-
-//-----------------------------------------------------------------------------
-// CWeaponPistol
-//-----------------------------------------------------------------------------
 
 class CWeaponTachi : public CWeaponHL2MPBase
 {
@@ -36,54 +25,14 @@ public:
 
 	CWeaponTachi(void);
 
+	Activity GetPrimaryAttackActivity(void);
+
 	DECLARE_NETWORKCLASS(); 
 	DECLARE_PREDICTABLE();
-
-	void	ItemPostFrame( void );
-	void	ItemPreFrame( void );
-	void	ItemBusyFrame( void );
-	void	PrimaryAttack( void );
-	void	SecondaryAttack(void);
-	void	AddViewKick( void );
-	void	DryFire( void );
-
-	void	UpdatePenaltyTime( void );
-
-	Activity	GetPrimaryAttackActivity( void );
-
-	virtual bool Reload( void );
-
-	virtual const Vector& GetBulletSpread( void )
-	{		
-		static Vector cone;
-
-		float ramp = RemapValClamped(	m_flAccuracyPenalty, 
-											0.0f, 
-											TACHI_ACCURACY_MAXIMUM_PENALTY_TIME, 
-											0.0f, 
-											1.0f ); 
-
-			// We lerp from very accurate to inaccurate over time
-		VectorLerp( VECTOR_CONE_1DEGREES, VECTOR_CONE_8DEGREES, ramp, cone );
-
-		return cone;
-	}
-
-	virtual float GetFireRate( void ) 
-	{
-		return 0.5f; 
-	}
 	
 #ifndef CLIENT_DLL
 	DECLARE_ACTTABLE();
 #endif
-
-private:
-	CNetworkVar( float,	m_flSoonestPrimaryAttack );
-	CNetworkVar( float,	m_flLastAttackTime );
-	CNetworkVar( float,	m_flAccuracyPenalty );
-	CNetworkVar( int,	m_nNumShotsFired );
-
 private:
 	CWeaponTachi( const CWeaponTachi & );
 };
@@ -91,25 +40,10 @@ private:
 IMPLEMENT_NETWORKCLASS_ALIASED( WeaponTachi, DT_WeaponTachi )
 
 BEGIN_NETWORK_TABLE( CWeaponTachi, DT_WeaponTachi )
-#ifdef CLIENT_DLL
-	RecvPropTime( RECVINFO( m_flSoonestPrimaryAttack ) ),
-	RecvPropTime( RECVINFO( m_flLastAttackTime ) ),
-	RecvPropFloat( RECVINFO( m_flAccuracyPenalty ) ),
-	RecvPropInt( RECVINFO( m_nNumShotsFired ) ),
-#else
-	SendPropTime( SENDINFO( m_flSoonestPrimaryAttack ) ),
-	SendPropTime( SENDINFO( m_flLastAttackTime ) ),
-	SendPropFloat( SENDINFO( m_flAccuracyPenalty ) ),
-	SendPropInt( SENDINFO( m_nNumShotsFired ) ),
-#endif
 END_NETWORK_TABLE()
 
 #ifdef CLIENT_DLL
 BEGIN_PREDICTION_DATA( CWeaponTachi )
-	DEFINE_PRED_FIELD( m_flSoonestPrimaryAttack, FIELD_FLOAT, FTYPEDESC_INSENDTABLE ),
-	DEFINE_PRED_FIELD( m_flLastAttackTime, FIELD_FLOAT, FTYPEDESC_INSENDTABLE ),
-	DEFINE_PRED_FIELD( m_flAccuracyPenalty, FIELD_FLOAT, FTYPEDESC_INSENDTABLE ),
-	DEFINE_PRED_FIELD( m_nNumShotsFired, FIELD_INTEGER, FTYPEDESC_INSENDTABLE ),
 END_PREDICTION_DATA()
 #endif
 
@@ -129,9 +63,7 @@ acttable_t CWeaponTachi::m_acttable[] =
 	{ ACT_RANGE_ATTACK1,				ACT_RANGE_ATTACK_PISTOL,				false },
 };
 
-
 IMPLEMENT_ACTTABLE( CWeaponTachi );
-
 #endif
 
 //-----------------------------------------------------------------------------
@@ -139,256 +71,20 @@ IMPLEMENT_ACTTABLE( CWeaponTachi );
 //-----------------------------------------------------------------------------
 CWeaponTachi::CWeaponTachi( void )
 {
-	m_flSoonestPrimaryAttack = gpGlobals->curtime;
-	m_flAccuracyPenalty = 0.0f;
-
+	m_iFireMode = FM_SEMI; //TODO: Switch to auto with +special
 	m_bFiresUnderwater	= true;
 }
 
-//-----------------------------------------------------------------------------
-// Purpose:
-//-----------------------------------------------------------------------------
-void CWeaponTachi::DryFire( void )
+Activity CWeaponTachi::GetPrimaryAttackActivity(void)
 {
-	WeaponSound( EMPTY );
-	SendWeaponAnim( ACT_VM_DRYFIRE );
-	
-	m_flSoonestPrimaryAttack	= gpGlobals->curtime + TACHI_FASTEST_DRY_REFIRE_TIME;
-	m_flNextPrimaryAttack		= gpGlobals->curtime + SequenceDuration();
-}
+	if (m_nShotsFired < 2)
+		return ACT_VM_PRIMARYATTACK; 
 
-//-----------------------------------------------------------------------------
-// Purpose:
-//-----------------------------------------------------------------------------
-void CWeaponTachi::PrimaryAttack( void )
-{
-	if ( ( gpGlobals->curtime - m_flLastAttackTime ) > 0.5f )
-	{
-		m_nNumShotsFired = 0;
-	}
-	else
-	{
-		m_nNumShotsFired++;
-	}
-
-	m_flLastAttackTime = gpGlobals->curtime;
-	m_flSoonestPrimaryAttack = gpGlobals->curtime + TACHI_FASTEST_REFIRE_TIME;
-
-	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
-
-	if( pOwner )
-	{
-		// Each time the player fires the pistol, reset the view punch. This prevents
-		// the aim from 'drifting off' when the player fires very quickly. This may
-		// not be the ideal way to achieve this, but it's cheap and it works, which is
-		// great for a feature we're evaluating. (sjb)
-		pOwner->ViewPunchReset();
-	}
-
-	BaseClass::PrimaryAttack();
-
-	// Add an accuracy penalty which can move past our maximum penalty time if we're really spastic
-	m_flAccuracyPenalty += TACHI_ACCURACY_SHOT_PENALTY_TIME;
-}
-
-void CWeaponTachi::SecondaryAttack(void)
-{
-	// Only the player fires this way so we can cast
-	CBasePlayer *pPlayer = ToBasePlayer(GetOwner());
-
-	if (!pPlayer)
-	{
-		return;
-	}
-
-	// No ammo
-	if ((UsesClipsForAmmo1() && m_iClip1 == 0) || (!UsesClipsForAmmo1() && !pPlayer->GetAmmoCount(m_iPrimaryAmmoType)))
-	{
-		m_flNextSecondaryAttack = gpGlobals->curtime + TACHI_FASTEST_DRY_REFIRE_TIME;
-
-		DryFire();
-
-		return;
-	}
-
-	int iBulletsToFire = 1; // Can change this to higher number for burst fire, just edit the rate of fire
-
-	if (iBulletsToFire > m_iClip1) // Shouldn't happen without more than one bullet
-	{
-		iBulletsToFire = m_iClip1;
-	}
-
-	// MUST call sound before removing a round from the clip of a CMachineGun
-	WeaponSound(SINGLE);
-
-	m_iClip1 -= iBulletsToFire;
-
-	pPlayer->DoMuzzleFlash();
-	SendWeaponAnim(ACT_VM_PRIMARYATTACK);
-
-	// player "shoot" animation
-	pPlayer->SetAnimation(PLAYER_ATTACK1);
-
-	if ((gpGlobals->curtime - m_flLastAttackTime) > 0.5f)
-	{
-		m_nNumShotsFired = 0;
-	}
-	else
-	{
-		m_nNumShotsFired++;
-	}
-
-	Vector vecSrc = pPlayer->Weapon_ShootPosition();
-	Vector vecAiming = pPlayer->GetAutoaimVector(AUTOAIM_5DEGREES);
-
-	FireBulletsInfo_t info(iBulletsToFire, vecSrc, vecAiming, GetBulletSpread(), MAX_TRACE_LENGTH, m_iPrimaryAmmoType);
-	info.m_pAttacker = pPlayer;
-
-	// Fire the bullets, and force the first shot to be perfectly accuracy
-	pPlayer->FireBullets(info);
-
-	if (!m_iClip1 && pPlayer->GetAmmoCount(m_iPrimaryAmmoType) <= 0)
-	{
-		// HEV suit - indicate out of ammo condition
-		pPlayer->SetSuitUpdate("!HEV_AMO0", FALSE, 0);
-	}
-
-	BaseClass::PrimaryAttack();
-
-	m_flNextPrimaryAttack = gpGlobals->curtime + TACHI_FASTEST_REFIRE_TIME;
-	m_flNextSecondaryAttack = gpGlobals->curtime + TACHI_FASTEST_REFIRE_TIME;
-
-	// Add an accuracy penalty which can move past our maximum penalty time if we're really spastic
-	m_flAccuracyPenalty += TACHI_ACCURACY_SHOT_PENALTY_TIME;
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: 
-//-----------------------------------------------------------------------------
-void CWeaponTachi::UpdatePenaltyTime( void )
-{
-	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
-
-	if ( pOwner == NULL )
-		return;
-
-	// Check our penalty time decay
-	if ( ( ( pOwner->m_nButtons & IN_ATTACK ) == false ) && ( m_flSoonestPrimaryAttack < gpGlobals->curtime ) )
-	{
-		m_flAccuracyPenalty -= gpGlobals->frametime;
-		m_flAccuracyPenalty = clamp( m_flAccuracyPenalty, 0.0f, TACHI_ACCURACY_MAXIMUM_PENALTY_TIME );
-	}
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: 
-//-----------------------------------------------------------------------------
-void CWeaponTachi::ItemPreFrame( void )
-{
-	UpdatePenaltyTime();
-
-	BaseClass::ItemPreFrame();
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: 
-//-----------------------------------------------------------------------------
-void CWeaponTachi::ItemBusyFrame( void )
-{
-	UpdatePenaltyTime();
-
-	BaseClass::ItemBusyFrame();
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: Allows firing as fast as button is pressed
-//-----------------------------------------------------------------------------
-void CWeaponTachi::ItemPostFrame( void )
-{
-	BaseClass::ItemPostFrame();
-
-	if ( m_bInReload )
-		return;
-	
-	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
-
-	if ( pOwner == NULL )
-		return;
-	
-	if ( pOwner->m_nButtons & IN_ATTACK2 )
-	{
-		m_flLastAttackTime = gpGlobals->curtime + TACHI_FASTEST_REFIRE_TIME;
-		m_flSoonestPrimaryAttack = gpGlobals->curtime + TACHI_FASTEST_REFIRE_TIME;
-		m_flNextPrimaryAttack = gpGlobals->curtime + TACHI_FASTEST_REFIRE_TIME;
-	}
-
-	//Allow a refire as fast as the player can click
-	if ( ( ( pOwner->m_nButtons & IN_ATTACK ) == false ) && ( m_flSoonestPrimaryAttack < gpGlobals->curtime ) )
-	{
-		m_flNextPrimaryAttack = gpGlobals->curtime - 0.1f;
-	}
-	else if ( ( pOwner->m_nButtons & IN_ATTACK ) && ( m_flNextPrimaryAttack < gpGlobals->curtime ) && ( m_iClip1 <= 0 ) )
-	{
-		DryFire();
-	}
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: 
-// Output : int
-//-----------------------------------------------------------------------------
-Activity CWeaponTachi::GetPrimaryAttackActivity( void )
-{
-	if ( m_nNumShotsFired < 1 )
-		return ACT_VM_PRIMARYATTACK;
-
-	if ( m_nNumShotsFired < 2 )
+	if (m_nShotsFired < 3)
 		return ACT_VM_RECOIL1;
 
-	if ( m_nNumShotsFired < 3 )
+	if (m_nShotsFired < 4)
 		return ACT_VM_RECOIL2;
 
 	return ACT_VM_RECOIL3;
-}
-
-//-----------------------------------------------------------------------------
-//-----------------------------------------------------------------------------
-bool CWeaponTachi::Reload( void )
-{
-	bool fRet = DefaultReload( GetMaxClip1(), GetMaxClip2(), ACT_VM_RELOAD );
-	if ( fRet )
-	{
-		WeaponSound( RELOAD );
-		m_flAccuracyPenalty = 0.0f;
-	}
-	return fRet;
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: 
-//-----------------------------------------------------------------------------
-void CWeaponTachi::AddViewKick( void )
-{
-	CBasePlayer *pPlayer  = ToBasePlayer( GetOwner() );
-
-	if (pPlayer == NULL)
-		return;
-
-	QAngle	viewPunch;
-
-	float maxpunchx = .5f;
-	float maxpunchy = .6f;
-
-	if (pPlayer->m_nButtons & IN_ATTACK2 && m_nNumShotsFired >= 4)
-	{
-		maxpunchx *= 2.5;
-		maxpunchy *= 2.5;
-	}
-
-	viewPunch.x = SharedRandomFloat("tachipax", .25, maxpunchx);
-	viewPunch.y = SharedRandomFloat("tachipay", -.5, maxpunchy);
-	viewPunch.z = 0.0f;
-
-	//Add it to the view punch
-	pPlayer->ViewPunch( viewPunch );
 }
