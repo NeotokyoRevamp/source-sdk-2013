b590eb7fa3f778db386ea85b205a153edb37fc66
soft as HELL <softashell@cock.li>
2015-07-16 13:33:13 +0300

    weapon_grenade: Add animations

--- mp/src/game/shared/hl2mp/weapon_grenade.cpp
+++ mp/src/game/shared/hl2mp/weapon_grenade.cpp
@@ -1,5 +1,6 @@
 #include "cbase.h"
 #include "weapon_hl2mpbase.h"
+#include "in_buttons.h"
 
 #if defined( CLIENT_DLL )
 #include "c_hl2mp_player.h"
@@ -28,7 +29,18 @@ public:
 
 	CWeaponGrenade();
 
+	void PrimaryAttack(void);
 	void SecondaryAttack(void) { return; }
+	void ItemPostFrame(void);
+
+	bool Deploy(void);
+
+private:
+	CNetworkVar(bool, m_bPinPulled);
+	CNetworkVar(bool, m_bThrownGrenade);
+
+	void ThrowGrenade(void);
+	void CreateGrenade(void);
 
 	CWeaponGrenade(const CWeaponGrenade &);
 };
@@ -36,10 +48,21 @@ public:
 IMPLEMENT_NETWORKCLASS_ALIASED(WeaponGrenade, DT_WeaponGrenade)
 
 BEGIN_NETWORK_TABLE(CWeaponGrenade, DT_WeaponGrenade)
+#ifdef CLIENT_DLL
+RecvPropInt(RECVINFO(m_bPinPulled)),
+RecvPropInt(RECVINFO(m_bThrownGrenade)),
+#else
+SendPropInt(SENDINFO(m_bPinPulled)),
+SendPropInt(SENDINFO(m_bThrownGrenade)),
+#endif
 END_NETWORK_TABLE()
 
+#ifdef CLIENT_DLL
 BEGIN_PREDICTION_DATA(CWeaponGrenade)
+DEFINE_PRED_FIELD(m_bPinPulled, FIELD_BOOLEAN, FTYPEDESC_INSENDTABLE),
+DEFINE_PRED_FIELD(m_bThrownGrenade, FIELD_BOOLEAN, FTYPEDESC_INSENDTABLE),
 END_PREDICTION_DATA()
+#endif
 
 LINK_ENTITY_TO_CLASS(weapon_grenade, CWeaponGrenade);
 PRECACHE_WEAPON_REGISTER(weapon_grenade);
@@ -64,5 +87,117 @@ IMPLEMENT_ACTTABLE(CWeaponGrenade);
 //-----------------------------------------------------------------------------
 CWeaponGrenade::CWeaponGrenade(void)
 {
+	m_bPinPulled = false;
+	m_bThrownGrenade = false;
+}
 
+void CWeaponGrenade::PrimaryAttack(void)
+{
+	// Only the player fires this way so we can cast
+	CBasePlayer *pOwner = ToBasePlayer(GetOwner());
+
+	if (!pOwner || m_bPinPulled)
+		return;
+
+	SendWeaponAnim(ACT_VM_PULLPIN);
+
+	m_flNextPrimaryAttack = gpGlobals->curtime + SequenceDuration();
+
+	m_bPinPulled = true;
+}
+
+void CWeaponGrenade::ItemPostFrame(void)
+{
+	CBasePlayer *pOwner = ToBasePlayer(GetOwner());
+	if (!pOwner)
+		return;
+
+	if (m_flNextPrimaryAttack <= gpGlobals->curtime)
+	{
+		// Throw animation ended and grenade is thrown
+		if (m_bThrownGrenade)
+		{
+			// Remove weapon
+			#ifndef CLIENT_DLL
+			UTIL_Remove(this);
+			#endif
+
+			// Switch to another weapon
+			pOwner->SwitchToNextBestWeapon(this);
+			return;
+		}
+		// Holding +attack
+		else if ((pOwner->m_nButtons & IN_ATTACK))
+		{
+			// Pin is not pulled yet
+			if (!m_bPinPulled)
+			{
+				// Show to pin pull animation
+				PrimaryAttack();
+			}
+			else
+			{
+				// Grenade has no pin pulled idle animation so we just hide the viewmodel here
+				SetWeaponVisible(false);
+			}
+
+			return;
+		}
+		// -attack and pin is pulled
+		else if (!(pOwner->m_nButtons & IN_ATTACK) && m_bPinPulled)
+		{
+			ThrowGrenade();
+			return;
+		}
+		else
+		{
+			// Nothing is happening
+			WeaponIdle();
+		}
+	}
+
+	BaseClass::ItemPostFrame();
+}
+
+// Reset the state when picking up weapon or switching to it
+bool CWeaponGrenade::Deploy()
+{
+	m_bPinPulled = false;
+
+	return BaseClass::Deploy();
+}
+
+// Spawn the grenade projectile and show throw animations
+void CWeaponGrenade::ThrowGrenade(void)
+{
+	CBasePlayer *pOwner = ToBasePlayer(GetOwner());
+	if (!pOwner)
+		return;
+
+	// Throw animation for player model
+	//pOwner->SetAnimation(PLAYER_ATTACK1);
+
+	// Show view model again
+	SetWeaponVisible(true);
+
+	// Show the throw animation
+	SendWeaponAnim(ACT_VM_THROW);
+
+	CreateGrenade();
+
+	m_flNextPrimaryAttack = gpGlobals->curtime + SequenceDuration();
+
+	m_bPinPulled = false;
+	m_bThrownGrenade = true;
+}
+
+void CWeaponGrenade::CreateGrenade(void)
+{
+	CBasePlayer *pOwner = ToBasePlayer(GetOwner());
+	if (!pOwner)
+		return;
+
+#ifndef CLIENT_DLL
+	// TODO: Create projectile
+#endif
 }
